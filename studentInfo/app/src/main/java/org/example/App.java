d1b520b4ce1ea495edf51fa765d85bd16436aa15
x2 <lunguj665@gmail.com>
2026-09-26 21:14:15 +0200

    test for inputs

--- studentInfo/app/src/main/java/org/example/App.java
+++ studentInfo/app/src/main/java/org/example/App.java
@@ -12,6 +12,7 @@ public class App extends Application {
 
   @Override
   public void start(Stage stage) {
+
     Home home = new Home();
     home.homePage(stage);
   }
--- studentInfo/app/src/main/java/org/example/views/Home.java
+++ studentInfo/app/src/main/java/org/example/views/Home.java
@@ -1,28 +1,34 @@
 package org.example.views;
 
+import javafx.scene.control.TextField;
+
 import javafx.geometry.Pos;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
 import javafx.scene.control.Label;
 import javafx.scene.layout.FlowPane;
+import javafx.scene.layout.HBox;
 import javafx.stage.Stage;
 
 public class Home {
   public void homePage(Stage stage) {
     Label label = new Label("Hello, JavaFX!");
-    Button mybtn = new Button("test");
+    Button mybtn = new Button("add");
+    var textField = new TextField("Hello World!");
     mybtn.setId("btn");
     mybtn.setOnAction(event -> {
-      label.setText("Button was clicked!");
+
+      label.setText(textField.getText());
     });
 
-    FlowPane root = new FlowPane(10, 10);
-    root.setAlignment(Pos.CENTER);
+    FlowPane root = new FlowPane(40, 10);
 
-    root.getChildren().addAll(label, mybtn);
+    root.setAlignment(Pos.CENTER);
+    root.getChildren().addAll(textField, mybtn, label);
 
     Scene scene = new Scene(root, 400, 300);
     scene.getStylesheets().add(getClass().getResource("css/Home.css").toExternalForm());
+
     stage.setTitle("My JavaFX App");
     stage.setScene(scene);
     stage.show();
